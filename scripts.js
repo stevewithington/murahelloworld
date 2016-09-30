80620a8c7e72574cc9966007bb1a8c1a65e1fbf3
Steve Withington <stephenwithington@gmail.com>
2016-09-30 13:56:11 -0500

    ui

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ Mura.DisplayObject.dsphelloworld = Mura.UI.extend({
     // objectparams (configurator settings) are available under 'this.context.{yourVar}'
     var helloworldtext = this.context.helloworldtext === undefined ? 'helloworldtext is not defined yet.' : this.context.helloworldtext;
 
-    Mura(this.context.targetEl).html('<div class="alert alert-success">' + helloworldtext + '</div>');
+    Mura(this.context.targetEl).html('<div class="alert alert-success"><h2><i class="fa fa-phone"></i> Hello World</h2>' + helloworldtext + '</div>');
   }
 
 });
