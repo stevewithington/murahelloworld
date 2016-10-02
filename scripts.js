4bd5ddf1487670e4977757287b54e25b974dd052
Steve Withington <stephenwithington@gmail.com>
2016-10-02 10:01:09 -0500

    updating ui heading

--- scripts.js
+++ scripts.js
@@ -1,13 +1,17 @@
 // Mura.DisplayObject.{display-object-directory-name}
 Mura.DisplayObject.murahelloworld = Mura.UI.extend({
 
-  // Mura invokes this method by default
+  // Mura invokes the 'render()' method by default
   render: function() {
 
     // objectparams (configurator settings) are available under 'this.context.{yourVar}'
-    var helloworldtext = this.context.helloworldtext === undefined ? 'helloworldtext is not defined yet.' : this.context.helloworldtext;
+    var helloworldtext =
+          this.context.helloworldtext === undefined
+            ? 'helloworldtext is not defined yet.'
+            : this.context.helloworldtext
+        , heading = '<h3><i class="fa fa-phone"></i> Hello World <small>(JS)</small></h3>';
 
-    Mura(this.context.targetEl).html('<div class="alert alert-success"><h2><i class="fa fa-phone"></i> Hello World</h2>' + helloworldtext + '</div>');
+    Mura(this.context.targetEl).html('<div class="alert alert-success">' + heading + helloworldtext + '</div>');
   }
 
 });
